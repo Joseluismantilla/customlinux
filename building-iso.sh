522204ad35a83c2189ef6d2a963e1c3d1b1e8fd0
Jose luis <kiosk@foundation0.ilt.example.com>
2020-02-09 09:19:29 -0500

    Updating centos 7.x version

---
+++ building-iso.sh
@@ -0,0 +1,39 @@
+
+
+mkdir /mnt/{dvd,iso}
+mount -o loop /path/to/some.iso /mnt/iso
+cp -r /tmp/iso/ /mnt/dvd
+umount /mnt/iso
+chmod -R u+w /mnt/iso
+
+cp /path/to/someks.cfg /tmp/dvd/isolinux/ks.cfg
+cp /path/to/*.rpm /tmp/dvd/Packages/.
+cd /tmp/dvd/Packages && createrepo -dpo .. .
+
+#Modifying boot list
+vim /tmp/dvd/isolinux/isolinux.cfg  #Updating the file with...
+label linux
+  menu label ^Install DevOps course - Instructor machine 
+  menu default
+  kernel vmlinuz
+  append initrd=initrd.img inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 inst.ks=cdrom:/ks.cfg
+
+label linux
+  menu label ^Install CentOS 7
+  kernel vmlinuz
+  append initrd=initrd.img inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 quiet
+
+label check
+  menu label Test this ^media & install CentOS 7
+  kernel vmlinuz
+  append initrd=initrd.img inst.stage2=hd:LABEL=CentOS\x207\x20x86_64 rd.live.check quiet
+
+
+cd /tmp/dvd  
+mkisofs -o /tmp/boot.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "CentOS 7 x86_64" -R -J -v -T isolinux/. .
+
+#(Optional) Use isohybrid if you want to dd the ISO file to a bootable USB key. 
+isohybrid /tmp/boot.iso
+
+#Add an MD5 checksum (to allow testing of media). 
+implantisomd5 /tmp/boot.iso
